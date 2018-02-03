6a391ae3b2f4418ec6bfc06db083e40f3dcebb14
jakipatryk <bajbartlomiej997@gmail.com>
2018-02-03 11:19:01 +0100

    Add getAuthorizationUrl function

---
+++ lib/auth/auth.spec.ts
@@ -0,0 +1,28 @@
+import 'mocha';
+import { expect } from 'chai';
+
+import { getAuthorizationUrl } from './auth';
+
+describe('getAuthorizationUrl', () => {
+
+    it('should return a correct url when scope has one element', () => {
+        const clientId = 'test.app';
+        const redirectUri = 'https://wykop.pl/redirect';
+        const scope = ['vote'];
+
+        const authorizationUrl = getAuthorizationUrl(clientId, redirectUri, scope);
+
+        expect(authorizationUrl).to.equal('https://steemconnect.com/oauth2/authorize?client_id=test.app&response_type=code&redirect_uri=https%3A%2F%2Fwykop.pl%2Fredirect&scope=vote');
+    });
+
+    it('should return a correct url when scope has more than one element', () => {
+        const clientId = 'test.app';
+        const redirectUri = 'https://adam.malysz/redirect';
+        const scope = ['vote', 'comment'];
+
+        const authorizationUrl = getAuthorizationUrl(clientId, redirectUri, scope);
+
+        expect(authorizationUrl).to.equal('https://steemconnect.com/oauth2/authorize?client_id=test.app&response_type=code&redirect_uri=https%3A%2F%2Fadam.malysz%2Fredirect&scope=vote%2Ccomment');
+    });
+
+});

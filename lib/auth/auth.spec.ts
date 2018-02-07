ca854c74c955efc0ee0873777f77a0e4a327fbd0
jakipatryk <bajbartlomiej997@gmail.com>
2018-02-07 18:22:19 +0100

    Add 'getAccesToken' function

--- lib/auth/auth.spec.ts
+++ lib/auth/auth.spec.ts
@@ -1,28 +1,91 @@
 import 'mocha';
 import { expect } from 'chai';
+import * as nock from 'nock';
 
-import { getAuthorizationUrl } from './auth';
+import { getAuthorizationUrl, getAccessToken } from './auth';
+
+import * as rp from 'request-promise';
 
 describe('getAuthorizationUrl', () => {
+  it('should return a correct url when scope has one element', () => {
+    const clientId = 'test.app';
+    const redirectUri = 'https://wykop.pl/redirect';
+    const scope = ['vote'];
 
-    it('should return a correct url when scope has one element', () => {
-        const clientId = 'test.app';
-        const redirectUri = 'https://wykop.pl/redirect';
-        const scope = ['vote'];
+    const authorizationUrl = getAuthorizationUrl(clientId, redirectUri, scope);
 
-        const authorizationUrl = getAuthorizationUrl(clientId, redirectUri, scope);
+    expect(authorizationUrl).to.equal(
+      'https://steemconnect.com/oauth2/authorize?client_id=test.app&response_type=code&redirect_uri=https%3A%2F%2Fwykop.pl%2Fredirect&scope=vote'
+    );
+  });
 
-        expect(authorizationUrl).to.equal('https://steemconnect.com/oauth2/authorize?client_id=test.app&response_type=code&redirect_uri=https%3A%2F%2Fwykop.pl%2Fredirect&scope=vote');
-    });
+  it('should return a correct url when scope has more than one element', () => {
+    const clientId = 'test.app';
+    const redirectUri = 'https://adam.malysz/redirect';
+    const scope = ['vote', 'comment'];
 
-    it('should return a correct url when scope has more than one element', () => {
-        const clientId = 'test.app';
-        const redirectUri = 'https://adam.malysz/redirect';
-        const scope = ['vote', 'comment'];
+    const authorizationUrl = getAuthorizationUrl(clientId, redirectUri, scope);
 
-        const authorizationUrl = getAuthorizationUrl(clientId, redirectUri, scope);
+    expect(authorizationUrl).to.equal(
+      'https://steemconnect.com/oauth2/authorize?client_id=test.app&response_type=code&redirect_uri=https%3A%2F%2Fadam.malysz%2Fredirect&scope=vote%2Ccomment'
+    );
+  });
+});
 
-        expect(authorizationUrl).to.equal('https://steemconnect.com/oauth2/authorize?client_id=test.app&response_type=code&redirect_uri=https%3A%2F%2Fadam.malysz%2Fredirect&scope=vote%2Ccomment');
-    });
+describe('getAccessToken', () => {
+  const clientId = 'steemconnecttest.app';
+  const clientSecret = 'gfd65464';
+  const redirectUri = 'https://adam.malysz/redirect';
+  const code = '435435435afsd';
 
+  it('should return an access token if code is valid', async () => {
+    nock('https://steemconnect.com')
+      .post('/api/oauth2/token')
+      .reply(200, {
+        username: 'dev',
+        access_token: {
+          access_token: 'ey.eyJy4MDI5NjU3fQ.lqX2bTkW7R',
+          expires_in: 604800,
+          username: 'dev'
+        }
+      });
+
+    const result = await getAccessToken(
+      clientId,
+      clientSecret,
+      redirectUri,
+      code
+    );
+
+    expect(result.username).to.equal('dev');
+    expect(result.access_token.access_token).to.equal(
+      'ey.eyJy4MDI5NjU3fQ.lqX2bTkW7R'
+    );
+  });
+
+  it('should return an error message if code is invalid', async () => {
+    nock('https://steemconnect.com')
+      .post('/api/oauth2/token')
+      .reply(401, {
+        error: 'invalid_grant',
+        error_description: 'The token has invalid role'
+      });
+
+    const result = await getAccessToken(
+      clientId,
+      clientSecret,
+      redirectUri,
+      code
+    );
+
+    expect(result.error).to.equal('invalid_grant');
+  });
+});
+
+describe('mintFirebaseToken', () => {
+  it('should mint a Firebase token based on given code');
+});
+
+describe('saveUserDataInFirestore', () => {
+  it('should save user data in the Firestore Document');
 });
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,20 +1,44 @@
 import * as qs from 'querystring';
+import * as rp from 'request-promise';
 
 export const getAuthorizationUrl = (
   clientId: string,
   redirectUri: string,
   scope: Array<string>
 ): string => {
+  const base = 'https://steemconnect.com/oauth2/authorize?';
+  const queryParams = {
+    client_id: clientId,
+    response_type: 'code',
+    redirect_uri: redirectUri,
+    scope: scope.join(',')
+  };
 
-    const base = 'https://steemconnect.com/oauth2/authorize?';
-    const queryParams = {
+  const endpoint = base + qs.stringify(queryParams);
+  return endpoint;
+};
+
+export const getAccessToken = async (
+  clientId: string,
+  clientSecret: string,
+  redirectUri: string,
+  code: string
+): Promise<any> => {
+  try {
+    const options = {
+      uri: 'https://steemconnect.com/api/oauth2/token',
+      form: {
+        grant_type: 'authorization_code',
         client_id: clientId,
-        response_type: 'code',
+        client_secret: clientSecret,
         redirect_uri: redirectUri,
-        scope: scope.join(',')
-      };
-    
-      const endpoint = base + qs.stringify(queryParams);
-      return endpoint;
-      
+        code: code
+      },
+      json: true
+    };
+    const accessToken = await rp.post(options);
+    return accessToken;
+  } catch (e) {
+    return e.response.body;
+  }
 };

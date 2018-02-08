5cb6594e2a1538f531ab6fcde4f23552146bb324
jakipatryk <bajbartlomiej997@gmail.com>
2018-02-08 21:38:23 +0100

    Add 'getAccessTokenFromFirestore' function

--- lib/auth/auth.spec.ts
+++ lib/auth/auth.spec.ts
@@ -7,7 +7,8 @@ import {
   getAuthorizationUrl,
   getAccessToken,
   mintFirebaseToken,
-  saveAccessToken
+  saveAccessToken,
+  getAccessTokenFromFirestore
 } from './auth';
 
 describe('getAuthorizationUrl', () => {
@@ -122,5 +123,52 @@ describe('saveAccessToken', () => {
         username: 'spaghetti'
       })
     ).to.be.true;
+  });
+});
+
+describe('getAccessTokenFromFirestore', () => {
+  it('should return access token if exists', async () => {
+    const Admin = function() {
+      this.firestore = () => this;
+      this.doc = path => this;
+      this.get = async () => {
+        return {
+          exists: true,
+          data: () => {
+            return {
+              access_token: 'fdsdfrerefv342.5435fd',
+              expires_in: 604800,
+              username: 'gg'
+            };
+          }
+        };
+      };
+    };
+    const admin = new Admin();
+    const uid = 'steemconnect:gg';
+
+    const accessToken = await getAccessTokenFromFirestore(admin, uid);
+
+    expect(accessToken).to.exist.and.include({
+      access_token: 'fdsdfrerefv342.5435fd'
+    });
+  });
+
+  it('should throw an error if token does not exist', () => {
+    const Admin = function() {
+      this.firestore = () => this;
+      this.doc = path => this;
+      this.get = async () => {
+        return {
+          exists: false
+        };
+      };
+    };
+    const admin = new Admin();
+    const uid = 'steemconnect:gg';
+
+    return getAccessTokenFromFirestore(admin, uid).catch(err => {
+      expect(err).to.exist;
+    });
   });
 });

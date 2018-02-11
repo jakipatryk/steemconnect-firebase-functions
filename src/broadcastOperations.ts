2bacbd3a0260066e5ad07bbd6163dcb8c45ed30c
jakipatryk <bajbartlomiej997@gmail.com>
2018-02-11 22:19:42 +0100

    Add return type of 'broadcastOperations'

--- src/broadcastOperations.ts
+++ src/broadcastOperations.ts
@@ -5,7 +5,7 @@ import * as rp from 'request-promise';
 export async function broadcastOperations(
   accessToken: string,
   operations: Operations
-) {
+): Promise<any> {
   try {
     const options = {
       uri: 'https://steemconnect.com/api/broadcast',

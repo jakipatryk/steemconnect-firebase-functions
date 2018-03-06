b50579bcf48884246f321cd85e1df37c7824fa6e
jakipatryk <bajbartlomiej997@gmail.com>
2018-03-06 22:11:07 +0100

    Fix missing parameter in `mintFirebaseToken` JSDocs

--- dist/mintFirebaseToken.js
+++ dist/mintFirebaseToken.js
@@ -11,6 +11,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * Creates and returns Firebase custom auth token.
  * @param admin The configurated firebase-admin object.
+ * @param {string} uid The uid of the user you want to mind custom Firebase auth token for.
  * @returns {Promise} Promise object that resolves into Firebase custom auth token.
  */
 function mintFirebaseToken(admin, uid) {

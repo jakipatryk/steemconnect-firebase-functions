2866618c51565464e2399b31360346c2b9c0ee1b
jakipatryk <bajbartlomiej997@gmail.com>
2018-03-06 21:04:53 +0100

    Fix lack of parameter in `broadcastUpvote` JSDocs

--- dist/broadcastUpvote.d.ts
+++ dist/broadcastUpvote.d.ts
@@ -4,6 +4,7 @@
  * @param {string} voter The username of the voter.
  * @param {string} author The username of the author of a comment/post that user wants to upvote.
  * @param {string} permlink The permlink of a comment/post that user wants to upvote.
+ * @param {number} weight The weight of the vote (ex. 10000 is going to broadcast a 100% upvote).
  * @returns {Promise} Promise object that resolves into the result of the operation.
  */
 export declare function broadcastUpvote(accessToken: string, voter: string, author: string, permlink: string, weight: number): Promise<any>;

8cfbce38d20473260871ebe7f945815251eeed33
jakipatryk <bajbartlomiej997@gmail.com>
2018-03-13 02:04:58 +0100

    Enable to add `state` variable

--- dist/getAuthorizationUrl.d.ts
+++ dist/getAuthorizationUrl.d.ts
@@ -3,6 +3,7 @@
  * @param {string} clientId The client id of the SteemConnect app.
  * @param {string} redirectUri The URI you want the user to be redirected to after successful login.
  * @param {Array} scope The array of scopes (ex. ['vote', 'comment']).
+ * @param {string} [state] Optional state variable
  * @returns {string} The URL of the SteemConnect OAuth2 authorization endpoint.
  */
-export declare function getAuthorizationUrl(clientId: string, redirectUri: string, scope: Array<string>): string;
+export declare function getAuthorizationUrl(clientId: string, redirectUri: string, scope: Array<string>, state?: string): string;
--- dist/getAuthorizationUrl.js
+++ dist/getAuthorizationUrl.js
@@ -6,15 +6,17 @@ const qs = require("querystring");
  * @param {string} clientId The client id of the SteemConnect app.
  * @param {string} redirectUri The URI you want the user to be redirected to after successful login.
  * @param {Array} scope The array of scopes (ex. ['vote', 'comment']).
+ * @param {string} [state] Optional state variable
  * @returns {string} The URL of the SteemConnect OAuth2 authorization endpoint.
  */
-function getAuthorizationUrl(clientId, redirectUri, scope) {
+function getAuthorizationUrl(clientId, redirectUri, scope, state) {
     const base = 'https://steemconnect.com/oauth2/authorize?';
     const queryParams = {
         client_id: clientId,
         response_type: 'code',
         redirect_uri: redirectUri,
-        scope: scope.join(',')
+        scope: scope.join(','),
+        state: state || null
     };
     const endpoint = base + qs.stringify(queryParams);
     return endpoint;

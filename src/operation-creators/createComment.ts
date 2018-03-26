8ce11d7a76a001c687be2c717eeced5fdf74c53e
jakipatryk <bajbartlomiej997@gmail.com>
2018-03-26 22:51:00 +0200

    Refactor `createComment`

--- src/operation-creators/createComment.ts
+++ src/operation-creators/createComment.ts
@@ -1,39 +1,34 @@
+import { createOperation } from './createOperation';
+import { CommentConfig } from './../interfaces/CommentConfig';
 import { Operation } from '../interfaces/Operation';
 
 /**
  * Creates and returns the comment operation.
- * @param {string} parentPermlink The parent permlink for comments or main tag for posts.
- * @param {string} author The author of the comment/post.
- * @param {string} permlink The permlink of the comment/post.
- * @param {string} body The content of the comment/post.
- * @param {string} [parentAuthor] The parent author of the comment.
- * @param {string} [title] The title of the post.
- * @param {object} [jsonMetadata] The additional metadata of the comment/post.
+ * @param {Object} config The configuration object for vote operation.
+ * @param {string} config.parent_permlink The parent permlink for comments or main tag for posts.
+ * @param {string} config.author The author of the comment/post.
+ * @param {string} config.permlink The permlink of the comment/post.
+ * @param {string} config.body The content of the comment/post.
+ * @param {string} [config.parent_author] The parent author of the comment.
+ * @param {string} [config.title] The title of the post.
+ * @param {string} [config.json_metadata] The additional metadata of the comment/post.
  * @returns {Array} A single comment operation in the form of an array.
  */
-export function createComment(
-  parentPermlink: string,
-  author: string,
-  permlink: string,
-  body: string,
-  parentAuthor?: string | null,
-  title?: string | null,
-  jsonMetadata?: object | null
-): Operation {
-  jsonMetadata = jsonMetadata || undefined;
-
-  const commentOperation: Operation = [
-    'comment',
-    {
-      parent_author: parentAuthor || '',
-      parent_permlink: parentPermlink,
-      author,
-      permlink,
-      title: title || '',
-      body,
-      json_metadata: JSON.stringify(jsonMetadata) || ''
-    }
-  ];
-
-  return commentOperation;
-}
+export const createComment = ({
+  parent_permlink,
+  author,
+  permlink,
+  body,
+  parent_author = '',
+  title = '',
+  json_metadata = ''
+}: CommentConfig): Operation =>
+  createOperation('comment', {
+    parent_permlink,
+    author,
+    permlink,
+    body,
+    parent_author,
+    title,
+    json_metadata
+  });

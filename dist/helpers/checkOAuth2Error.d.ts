2a6f8e5d88a9a6ee11e37809167dccc03a78529b
jakipatryk <bajbartlomiej997@gmail.com>
2018-03-29 17:50:15 +0200

    Fix JSDocs wrong type

--- dist/helpers/checkOAuth2Error.d.ts
+++ dist/helpers/checkOAuth2Error.d.ts
@@ -2,7 +2,7 @@ import { OAuth2Error } from '../interfaces/OAuth2Error';
 /**
  * Checks if OAuth2 error is equal to provided error.
  * @param {Object} oAuth2Error The object of OAuth2 error.
- * @param {string} errorToCheckAgainst The error constant to check whether it is a provided OAuth2 error.
+ * @param {Object} errorToCheckAgainst The error constant to check whether it is a provided OAuth2 error.
  * @returns {boolean} Returns boolean determining whether provided OAuth2 error is the errorToCheck.
  */
 export declare const checkOAuth2Error: ({ error, error_description }: OAuth2Error, errorToCheckAgainst: OAuth2Error) => boolean;

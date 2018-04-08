cbb6e557ce0da23c9317c8ecf61c39e79076171d
jakipatryk <bajbartlomiej997@gmail.com>
2018-04-08 22:21:08 +0200

    Remake `broadcastPost`

--- src/broadcasting/broadcastPost.ts
+++ src/broadcasting/broadcastPost.ts
@@ -1,38 +1,22 @@
-import * as broadcaster from './broadcastOperations';
-import { createComment } from '../operation-creators/createComment';
+import { createBroadcastableComment } from '../shared/helpers/createBroadcastable';
+import { Post } from './interfaces/Post';
+import { AccessTokenResponse } from './../shared/interfaces/AccessTokenResponse';
+import { BroadcastResult } from './interfaces/BroadcastResult';
 
-import { Operation } from '../interfaces/Operation';
-import { BroadcastResult } from './../interfaces/BroadcastResult';
-
-/**
- * Broadcasts a post to the Steem blockchain and returns the result of the operation.
- * @param {string} accessToken The access_token of the user.
- * @param {string} mainTag The main tag of the post (not possible to change it later).
- * @param {string} postAuthor The username of the user who wants to add a post.
- * @param {string} postPermlink The permlink of the post.
- * @param {string} postTitle The title of the post.
- * @param {string} postBody The content of the post.
- * @param {Object} [jsonMetadata] Optional additional metadata (ex. the name of the app or additional tags).
- * @returns {Promise} Promise object that resolves into the result of the operation.
- */
-export function broadcastPost(
-  accessToken: string,
-  mainTag: string,
-  postAuthor: string,
-  postPermlink: string,
-  postTitle: string,
-  postBody: string,
-  jsonMetadata?: object
-): Promise<BroadcastResult> {
-  const json = JSON.stringify(jsonMetadata) || '';
-  const operation: Operation = createComment({
+export const broadcastPost = ({
+  mainTag,
+  author,
+  permlink,
+  title,
+  body,
+  metadata
+}: Post) => ({ access_token }: AccessTokenResponse): BroadcastResult =>
+  createBroadcastableComment({
     parent_permlink: mainTag,
-    author: postAuthor,
-    permlink: postPermlink,
-    body: postBody,
-    title: postTitle,
-    json_metadata: json
-  });
-
-  return broadcaster.broadcastOperations(accessToken, [operation]);
-}
+    author: author,
+    permlink: permlink,
+    body: body,
+    parent_author: '',
+    title: title,
+    json_metadata: JSON.stringify(metadata)
+  })({ access_token });
--- tests/broadcasting/broadcastPost.spec.ts
+++ tests/broadcasting/broadcastPost.spec.ts
@@ -1,84 +1,102 @@
 import { broadcastPost } from '../../src/broadcasting/broadcastPost';
-import * as broadcaster from '../../src/broadcasting/broadcastOperations';
 
 import { expect } from 'chai';
-import * as sinon from 'sinon';
+import * as nock from 'nock';
 
-describe('broadcastComment', function() {
-  const accessToken = 'fdsgfdew';
-  const mainTag = 'maintag';
-  const postAuthor = 'jakiaptryk-dev';
-  const postPermlink = 'ftyfyytvgyb67';
-  const postTitle = 'nice-title';
-  const postBody = 'post content';
+describe('broadcastPost', function() {
+  const accessToken = {
+    access_token: 'fdsgfdew',
+    expires_in: 34234,
+    username: 'ned'
+  };
 
-  beforeEach(function() {
-    this.broadcastOperations = sinon.stub(broadcaster, 'broadcastOperations');
-  });
+  const post = {
+    mainTag: 'test-tag',
+    author: 'whoever',
+    permlink: 'some-permlink',
+    title: 'title',
+    body: 'some post'
+  };
+  const metadata = {
+    anything: 'blabla'
+  };
 
-  it('should call broadcastOperations with correct data of the comment to broadcast if jsonMetadata provided', async function() {
-    const jsonMetadata = {
-      anything: 'blabla'
-    };
+  it('should broadcast post and return result if comment metadata provided', async function() {
+    nock('https://steemconnect.com')
+      .post('/api/broadcast')
+      .reply(200, (uri, req) => ({
+        result: {
+          id: '43523bhbbv543hv5345',
+          block_num: 6546765,
+          trx_num: 1,
+          expired: false,
+          ref_block_num: 43223,
+          ref_block_prefix: 654645645,
+          expiration: '2018-02-11T01:12:42',
+          operations: req.operations,
+          extensions: [],
+          signatures: [
+            '4234bjhbhjbhjbhjbh5jb4325uiph235io45ioh3b45ug435b3hb5j3m5k23nm5j3nih5b34h5bn32on'
+          ]
+        }
+      }));
+    const postWithMetadata = Object.assign({}, post, { metadata });
+
+    const result = await broadcastPost(postWithMetadata)(accessToken);
+
     const operations = [
       [
         'comment',
         {
           parent_author: '',
-          parent_permlink: mainTag,
-          author: postAuthor,
-          permlink: postPermlink,
-          title: postTitle,
-          body: postBody,
-          json_metadata: JSON.stringify(jsonMetadata)
+          parent_permlink: 'test-tag',
+          author: 'whoever',
+          permlink: 'some-permlink',
+          title: 'title',
+          body: 'some post',
+          json_metadata: JSON.stringify(metadata)
         }
       ]
     ];
-
-    await broadcastPost(
-      accessToken,
-      mainTag,
-      postAuthor,
-      postPermlink,
-      postTitle,
-      postBody,
-      jsonMetadata
-    );
-
-    expect(this.broadcastOperations.calledWith(accessToken, operations)).to.be
-      .true;
+    expect(result.result.operations).to.deep.equal(operations);
   });
 
-  it('should call broadcastOperations with correct data of the comment to broadcast if jsonMetadata NOT provided', async function() {
+  it('should broadcast post and return result if comment metadata NOT provided', async function() {
+    nock('https://steemconnect.com')
+      .post('/api/broadcast')
+      .reply(200, (uri, req) => ({
+        result: {
+          id: '43523bhbbv543hv5345',
+          block_num: 6546765,
+          trx_num: 1,
+          expired: false,
+          ref_block_num: 43223,
+          ref_block_prefix: 654645645,
+          expiration: '2018-02-11T01:12:42',
+          operations: req.operations,
+          extensions: [],
+          signatures: [
+            '4234bjhbhjbhjbhjbh5jb4325uiph235io45ioh3b45ug435b3hb5j3m5k23nm5j3nih5b34h5bn32on'
+          ]
+        }
+      }));
+
+    const result = await broadcastPost(post)(accessToken);
+
     const operations = [
       [
         'comment',
         {
           parent_author: '',
-          parent_permlink: mainTag,
-          author: postAuthor,
-          permlink: postPermlink,
-          title: postTitle,
-          body: postBody,
+          parent_permlink: 'test-tag',
+          author: 'whoever',
+          permlink: 'some-permlink',
+          title: 'title',
+          body: 'some post',
           json_metadata: ''
         }
       ]
     ];
-
-    await broadcastPost(
-      accessToken,
-      mainTag,
-      postAuthor,
-      postPermlink,
-      postTitle,
-      postBody
-    );
-
-    expect(this.broadcastOperations.calledWith(accessToken, operations)).to.be
-      .true;
-  });
-
-  afterEach(function() {
-    this.broadcastOperations.restore();
+    expect(result.result.operations).to.deep.equal(operations);
   });
 });

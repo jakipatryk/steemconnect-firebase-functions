eb18a5e44f46f6188ffc78348cca95e10a07995c
jakipatryk <bajbartlomiej997@gmail.com>
2018-04-08 22:22:35 +0200

    Remake `broadcastUnfollow`

--- dist/broadcasting/broadcastUnfollow.js
+++ dist/broadcasting/broadcastUnfollow.js
@@ -1,28 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const broadcaster = require("./broadcastOperations");
-const createCustomJson_1 = require("../operation-creators/createCustomJson");
-/**
- * Broadcasts a comment to the Steem blockchain and returns the result of the operation.
- * @param {string} accessToken The access_token of the user.
- * @param {string} username The username of the user who wants to unfollow somebody.
- * @param {string} userToUnfollow The username of the user to unfollow.
- * @returns {Promise} Promise object that resolves into the result of the operation.
- */
-function broadcastUnfollow(accessToken, username, userToUnfollow) {
-    const customJson = JSON.stringify([
+const createBroadcastable_1 = require("../shared/helpers/createBroadcastable");
+exports.broadcastUnfollow = ({ username, userToUnfollow }) => ({ access_token }) => createBroadcastable_1.createBroadcastableCustomJson({
+    required_posting_auths: [username],
+    id: 'follow',
+    json: JSON.stringify([
         'follow',
         {
             follower: username,
             following: userToUnfollow,
             what: []
         }
-    ]);
-    const operation = createCustomJson_1.createCustomJson({
-        required_posting_auths: [username],
-        id: 'follow',
-        json: customJson
-    });
-    return broadcaster.broadcastOperations(accessToken, [operation]);
-}
-exports.broadcastUnfollow = broadcastUnfollow;
+    ])
+})({ access_token });

6f197c31e9f30673060f6e27deb2144239440cb8
jakipatryk <bajbartlomiej997@gmail.com>
2018-04-08 22:20:03 +0200

    Remake `broadcastDownvote`

--- tests/broadcasting/broadcastDownvote.spec.ts
+++ tests/broadcasting/broadcastDownvote.spec.ts
@@ -1,20 +1,41 @@
 import { broadcastDownvote } from '../../src/broadcasting/broadcastDownvote';
-import * as broadcaster from '../../src/broadcasting/broadcastOperations';
 
 import { expect } from 'chai';
-import * as sinon from 'sinon';
+import * as nock from 'nock';
 
 describe('broadcastDownvote', function() {
-  beforeEach(function() {
-    this.broadcastOperations = sinon.stub(broadcaster, 'broadcastOperations');
-  });
+  const voteConfig = {
+    voter: 'jakipatrikko',
+    author: 'whoever',
+    permlink: 'some-permlink',
+    weight: 10000
+  };
+  const accessToken = {
+    access_token: 'fdsfertre',
+    expires_in: 4323432,
+    username: 'ned'
+  };
 
-  it('should call broadcastOperations with correct data', async function() {
-    const accessToken = 'fdsgfdew';
-    const voter = 'jakipatrikko';
-    const author = 'whoever';
-    const permlink = 'some-permlink';
-    const weight = 500;
+  it('should broadcast downvote and return result', async function() {
+    nock('https://steemconnect.com')
+      .post('/api/broadcast')
+      .reply(200, (uri, req) => ({
+        result: {
+          id: '43523bhbbv543hv5345',
+          block_num: 6546765,
+          trx_num: 1,
+          expired: false,
+          ref_block_num: 43223,
+          ref_block_prefix: 654645645,
+          expiration: '2018-02-11T01:12:42',
+          operations: req.operations,
+          extensions: [],
+          signatures: [
+            '4234bjhbhjbhjbhjbh5jb4325uiph235io45ioh3b45ug435b3hb5j3m5k23nm5j3nih5b34h5bn32on'
+          ]
+        }
+      }));
+
     const operations = [
       [
         'vote',
@@ -22,18 +43,14 @@ describe('broadcastDownvote', function() {
           voter: 'jakipatrikko',
           author: 'whoever',
           permlink: 'some-permlink',
-          weight: -500
+          weight: -10000
         }
       ]
     ];
 
-    await broadcastDownvote(accessToken, voter, author, permlink, weight);
+    const broadcastResult = await broadcastDownvote(voteConfig)(accessToken);
 
-    expect(this.broadcastOperations.calledWith(accessToken, operations)).to.be
-      .true;
-  });
-
-  afterEach(function() {
-    this.broadcastOperations.restore();
+    expect(broadcastResult).to.have.property('result');
+    expect(broadcastResult.result.operations).to.deep.equal(operations);
   });
 });

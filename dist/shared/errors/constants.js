313e78eac5969ab94ccbaab0336f68e6100984d6
jakipatryk <bajbartlomiej997@gmail.com>
2018-04-09 14:58:58 +0200

    Add JSDocs for error constants

--- dist/shared/errors/constants.js
+++ dist/shared/errors/constants.js
@@ -1,21 +1,46 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * @constant
+ * @type {Object}
+ * @default
+ */
 exports.ACCESS_TOKEN_EXPIRED = Object.freeze({
     error: 'invalid_grant',
     error_description: 'The token has invalid role'
 });
+/**
+ * @constant
+ * @type {Object}
+ * @default
+ */
 exports.ACCESS_TOKEN_INVALID = Object.freeze({
     error: 'invalid_grant',
     error_description: 'The token has invalid role'
 });
+/**
+ * @constant
+ * @type {Object}
+ * @default
+ */
 exports.ACCESS_TOKEN_REVOKED = Object.freeze({
     error: 'invalid_grant',
     error_description: 'The access_token has been revoked'
 });
+/**
+ * @constant
+ * @type {Object}
+ * @default
+ */
 exports.CODE_INVALID = Object.freeze({
     error: 'invalid_grant',
     error_description: 'The token has invalid role'
 });
+/**
+ * @constant
+ * @type {Object}
+ * @default
+ */
 exports.REFRESH_TOKEN_INVALID = Object.freeze({
     error: 'invalid_grant',
     error_description: 'The token has invalid role'

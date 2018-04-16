7f8dd111cb06c3461b195f567c21377d553df65c
jakipatryk <bajbartlomiej997@gmail.com>
2018-04-16 13:23:10 +0200

    Remove JSDocs

--- src/firebase/mintFirebaseToken.ts
+++ src/firebase/mintFirebaseToken.ts
@@ -1,9 +1,3 @@
-/**
- * Creates and returns Firebase custom auth token.
- * @param admin The configurated firebase-admin object.
- * @param {string} uid The uid of the user you want to mind custom Firebase auth token for.
- * @returns {Promise} Promise object that resolves into Firebase custom auth token.
- */
 export async function mintFirebaseToken(
   admin: any,
   uid: string

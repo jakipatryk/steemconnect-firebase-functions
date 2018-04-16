ddd674c8058f55054f2991040a7aab8866ea1ace
jakipatryk <bajbartlomiej997@gmail.com>
2018-04-16 17:06:10 +0200

    Add missing files

---
+++ dist/broadcasting/interfaces/index.d.ts
@@ -0,0 +1,8 @@
+export { BroadcastResult } from './BroadcastResult';
+export { Comment } from './Comment';
+export { Follow } from './Follow';
+export { Post } from './Post';
+export { PostWithBeneficiaries } from './PostWithBeneficiaries';
+export { Reblog } from './Reblog';
+export { Unfollow } from './Unfollow';
+export { Vote } from './Vote';

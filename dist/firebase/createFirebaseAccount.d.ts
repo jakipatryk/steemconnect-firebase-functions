1cefac7fe7537c1a916fcd06ffcd4b8df8d5564b
jakipatryk <bajbartlomiej997@gmail.com>
2018-04-16 13:22:51 +0200

    Refactor `createFirebaseAccount`

--- dist/firebase/createFirebaseAccount.d.ts
+++ dist/firebase/createFirebaseAccount.d.ts
@@ -1,13 +1,2 @@
-/**
- * Creates or updates Firebase auth user account.
- * @param admin The configurated admin object.
- * @param {string} uid The uid of the user.
- * @param {string} username The username of the user.
- * @param {string} [photoURL] Optional user's photo URL.
- * @param {string} [email] Optional user's email.
- * @param {boolean} [emailVerified] Optional boolean whether or not the user's email is verified.
- * @param {string} [phoneNumber] Optional user's phone number.
- * @param {boolean} [disabled] Optional boolean whether or not the user is disabled.
- * @returns {Promise} Promise object.
- */
-export declare function createFirebaseAccount(admin: any, uid: string, username: string, photoURL?: string, email?: string, emailVerified?: boolean, phoneNumber?: string, disabled?: boolean): Promise<any>;
+import { FirebaseAccount } from './interfaces/FirebaseAccount';
+export declare function createFirebaseAccount(admin: any, {uid, username, photoURL, email, emailVerified, phoneNumber, disabled}: FirebaseAccount): Promise<any>;

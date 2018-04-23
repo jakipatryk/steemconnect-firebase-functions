a6924d86b3c0e2af953d2fa62e307afa49598fb7
jakipatryk <bajbartlomiej997@gmail.com>
2018-04-23 17:40:38 +0200

    Make `ClientCredentials` fields required in `getAccessToken`

--- dist/oauth2/getAccessToken.d.ts
+++ dist/oauth2/getAccessToken.d.ts
@@ -1,6 +1,6 @@
 import { ClientCredentials } from './interfaces/ClientCredentials';
 import { AccessTokenResponse } from '../shared/interfaces/AccessTokenResponse';
-export declare function getAccessToken({clientId, clientSecret, redirectUri, code}: ClientCredentials & {
+export declare function getAccessToken({clientId, clientSecret, redirectUri, code}: Required<ClientCredentials> & {
     redirectUri: string;
     code: string;
 }): Promise<AccessTokenResponse>;
--- src/oauth2/getAccessToken.ts
+++ src/oauth2/getAccessToken.ts
@@ -8,7 +8,7 @@ export async function getAccessToken({
   clientSecret,
   redirectUri,
   code
-}: ClientCredentials & {
+}: Required<ClientCredentials> & {
   redirectUri: string;
   code: string;
 }): Promise<AccessTokenResponse> {

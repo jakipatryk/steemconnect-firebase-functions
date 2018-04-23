cb53068404053e0d041adaf535db55018064aa2f
jakipatryk <bajbartlomiej997@gmail.com>
2018-04-23 17:49:47 +0200

    Remove JSDocs

--- dist/operation-creators/createOperation.d.ts
+++ dist/operation-creators/createOperation.d.ts
@@ -1,10 +1,4 @@
 import { Operation } from '../shared/interfaces/Operation';
-/**
- * Creates and returns operation.
- * @param {string} type The type of the operation (ex. 'comment').
- * @param {Object} config The configuration object for the operation.
- * @returns A single operation in the form of an array.
- */
 export declare const createOperation: (type: string, { ...parameters }: {
     [x: string]: any;
 }) => Operation;
--- src/operation-creators/createOperation.ts
+++ src/operation-creators/createOperation.ts
@@ -1,11 +1,5 @@
 import { Operation } from '../shared/interfaces/Operation';
 
-/**
- * Creates and returns operation.
- * @param {string} type The type of the operation (ex. 'comment').
- * @param {Object} config The configuration object for the operation.
- * @returns A single operation in the form of an array.
- */
 export const createOperation = (type: string, { ...parameters }): Operation => [
   type,
   { ...parameters }

63c743c217cd7cb8d5cf277cca438bc271f36c65
jakipatryk <bajbartlomiej997@gmail.com>
2018-04-23 17:37:56 +0200

    Fix interfaces exports

--- dist/oauth2/index.d.ts
+++ dist/oauth2/index.d.ts
@@ -4,3 +4,4 @@ export { getUserData } from './getUserData';
 export { refreshAccessToken } from './refreshAccessToken';
 export { revokeAccessToken } from './revokeAccessToken';
 export { setUserMetadata } from './setUserMetadata';
+export * from './interfaces';
